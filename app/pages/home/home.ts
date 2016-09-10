65181da6dbbec30d31f3edd10477053e4fc3e403
Pietro Sacco <pietro.sacco@bbv.ch>
2016-09-10 22:02:06 +0200

    Imported MyData in home.ts.

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MyData } from '../../providers/my-data/my-data';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+  templateUrl: 'build/pages/home/home.html',
+  providers: [MyData]
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
 
+  private taskList;
+
+  constructor(public navCtrl: NavController, private dataService: MyData) {
+    dataService.getData().then(data => this.taskList = data);
   }
 }
