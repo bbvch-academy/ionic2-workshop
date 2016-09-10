23546b48211c0fe6e38c597a9435ee9628d0f609
Pietro Sacco <pietro.sacco@bbv.ch>
2016-09-10 21:47:41 +0200

    Added data.json file. Read json in my-data provider.

--- app/providers/my-data/my-data.ts
+++ app/providers/my-data/my-data.ts
@@ -12,6 +12,20 @@ import 'rxjs/add/operator/map';
 export class MyData {
 
   constructor(private http: Http) {}
-
+  
+  getData() {
+    return new Promise(resolve => {
+      // We're using Angular Http provider to request the data,
+      // then on the response it'll map the JSON data to a parsed JS object.
+      // Next we process the data and resolve the promise with the new data.
+      this.http.get('assets/data.json')
+        .map(res => res.json())
+        .subscribe(data => {
+          // we've got back the raw data, now generate the core schedule data
+          // and save the data for later reference
+          resolve(data);
+        });
+    });
+  }
 }
 
