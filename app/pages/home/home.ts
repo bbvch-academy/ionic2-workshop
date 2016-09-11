8fd76cf32cd671ec162e886d9e48738180a41d44
Pietro Sacco <pietro.sacco@bbv.ch>
2016-09-11 09:05:41 +0200

    Pushing item-detail-page onto the navigation stack by tapping on item.

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, LoadingController, Loading, Refresher } from 'ionic-angular';
 import { MyData } from '../../providers/my-data/my-data';
 //import { TaskListModel } from '../../models/task-list-model';
+import { ItemDetailPage } from '../item-detail/item-detail';
 
 @Component({
   templateUrl: 'build/pages/home/home.html',
@@ -40,7 +41,7 @@ export class HomePage {
 
   itemTapped($event: MouseEvent, item) {
     console.log('itemTapped '+event);
-    
+    this.navCtrl.push(ItemDetailPage, item);
   }
 
   checkboxTapped($event: MouseEvent, item) {
